ceedca66e2d9127c122ae25e17546903b180313b
Vasil Genov <vg1@Vasils-MacBook-Pro.local>
2025-01-02 16:15:14 +0200

    gitignore

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,10 @@
 use std::io;
 
 fn main() {
+    let x = 5;
+    let y = 10;
+    println!("x is {x}, y is {}, soo x + y = {}", y, x + y);
+
     println!("Guess the number!");
     println!("Please input your guess.");
 
@@ -10,5 +14,5 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed {guess}");
+    println!("You guessed {}", guess);
 }
